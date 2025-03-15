747b9dcdc73a97dd6b156479a88d8695d90102d2
iamjohnnysky <vanpasie20@gmail.com>
2025-03-15 15:42:27 -0700

    tailwind finally working perfect

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,13 @@
-
 module.exports = {
-  purge: [],
+  content: ["./public/**/*.html", "./src/**/*.{js,ts,jsx,tsx}"], // Make sure all relevant files are included
   theme: {
     extend: {
       colors: {
         primary: '#FF6363',
-        secondary: {
-          100: '#E2E2D5',
-          200: '#888883',
-        }
+        "secondary-100": "#E2E2D5",
+        "secondary-200": "#888883",
       },
-      fontFamily: {
-        body: ['Nunito']
-      }
     },
   },
-  variants: {},
   plugins: [],
-}
+};
